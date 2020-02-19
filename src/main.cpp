f6f25b3bd134ed32c294f0221cf75b2f890da4a4
JYL <leejy31415@gmail.com>
2020-02-19 15:49:11 +0900

    update main

--- src/main.cpp
+++ src/main.cpp
@@ -3,22 +3,22 @@
 int main()
 {
     int degree;
-	float start;
-	std::optional<float> result;
+    float start;
+    std::optional<float> result;
 
     std::cout << "Enter the degree of polynomial: ";
     std::cin >> degree;
     Polynomial f(degree);
 
-	std::cout << "Enter initial guess: ";
-	std::cin >> start;
+    std::cout << "Enter initial guess: ";
+    std::cin >> start;
 
-	std::cout << "Solving " << f << "=0 using Newton's method starting at x0 = " << start << '\n';
-	std::cout << "Result\n";
-	result = NewtonMethod(f, start);
+    std::cout << "Solving " << f << "=0 using Newton's method starting at x0 = " << start << '\n';
+    std::cout << "Result\n";
+    result = NewtonMethod(f, start);
 
-	if (result)
-		std::cout << "x = " << result.value() << '\n';
+    if (result)
+        std::cout << "x = " << result.value() << '\n';
 
     return 0;
 }
